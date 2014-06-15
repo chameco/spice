d684674cdadbc419e09f8d8c78856236949d83b9
Samuel Breese <chameco@tfwno.gf>
2014-06-15 11:05:57 -0400

    Updated

--- spice.scm
+++ spice.scm
@@ -1,5 +1,5 @@
-(module spice (get-stock-prices-range mean get-average-stock-price-range get-friday-stock-prices-range get-average-stock-price get-yearly-summary differences population-standard-deviation sample-standard-deviation)
- (import chicken scheme (srfi 1))
+;(module spice (get-stock-prices-range mean get-average-stock-price-range get-friday-stock-prices-range get-average-stock-price get-yearly-summary differences population-standard-deviation sample-standard-deviation)
+ (import chicken scheme (srfi 1) utils)
  (use s bricko)
 
  (define month-days '#(31 28 31 30 31 30 31 31 30 31 30 31))
@@ -13,18 +13,35 @@
      (string->number (cadr (fetch-contents '(close) q)))))
 
  (define (get-stock-prices-range selector symbol start end)
-   (let ([t (http-get (string-append "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%3D%22"
-                                     symbol "%22%20and%20startDate%3D%22" start "%22%20and%20endDate%3D%22" end "%22&env=store://datatables.org/alltableswithkeys"))])
-     (map selector (cdr (fetch-contents '(query results) t)))))
+   (let* ([t (http-get (string-append "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%3D%22"
+                                     symbol "%22%20and%20startDate%3D%22" start "%22%20and%20endDate%3D%22" end "%22&env=store://datatables.org/alltableswithkeys"))]
+          [contents (fetch-contents '(query results) t)])
+     (map selector (cdr contents))))
 
  (define (mean lst)
-   (let ([l (length lst)])
-    (if (= l 0)
-      0
-      (/ (reduce + 0 lst) l))))
+   (if lst
+     (let ([l (length lst)])
+      (if (= l 0)
+        0
+        (/ (reduce + 0 lst) l)))
+     #f))
+
+ (define (differences lst)
+   (if (null? lst)
+     #f
+     (if (null? (cdr lst))
+       '()
+       (cons (abs (- (car lst) (cadr lst))) (differences (cdr lst))))))
 
  (define (get-average-stock-price-range symbol start end)
    (mean (get-stock-prices-range get-closing symbol start end)))
+
+ (define (get-stock-stability-range symbol start end)
+   (let* ([l (get-stock-prices-range get-closing symbol start end)]
+          [md (mean (differences l))])
+     (if md
+       (/ md (mean l))
+       #f)))
 
  (define (get-month dstring)
    (string->number (cadr (s-split "-" dstring))))
@@ -68,11 +85,6 @@
 
  (define (square x) (* x x))
 
- (define (differences lst)
-   (if (null? (cdr lst))
-     '()
-     (cons (abs (- (car lst) (cadr lst))) (differences (cdr lst)))))
-
  (define (general-standard-deviation lst length-func)
    (let* ([m (mean lst)]
           [sq-dev (map (lambda (x) (square (- x m))) lst)]
@@ -80,4 +92,39 @@
      (sqrt variance)))
 
  (define (population-standard-deviation lst) (general-standard-deviation lst length))
- (define (sample-standard-deviation lst) (general-standard-deviation lst (lambda (lst) (- (length lst) 1)))))
+ (define (sample-standard-deviation lst) (general-standard-deviation lst (lambda (lst) (- (length lst) 1))))
+
+ (define (debug x)
+   (print x)
+   x)
+
+ (define (get-all-net-worths)
+   (let loop ([p (http-get "http://archive.fortune.com/magazines/fortune/fortune500/2010/snapshots/2255.html")]
+              [i 0])
+     (if (< i 500)
+       (cons 
+         (debug (cons
+                  (cadr (reverse (s-split "-" (apply string-append (cdr (fetch-contents '(html head meta title) p))))))
+                  (string->number (list->string (remove (lambda (x) (eqv? #\, x)) (string->list (s-trim (caddr (fetch-contents '(tbody (tr 4) (td 1)) (get-by-id "snapTable1" p))))))))))
+         (loop (http-get (string-append "http://archive.fortune.com/magazines/fortune/fortune500/2010/snapshots/" (get-attr 'href (fetch-contents '((a 1)) (get-by-id "slidesNav" p)))))
+               (+ i 1)))
+       '())))
+
+ (define (associate-net-worths-with-prices nw)
+   (let loop ([l nw])
+    (if (null? l)
+      '()
+      (cons (debug (list (caar l) (cdar l) (get-stock-stability-range (caar l) "2010-01-01" "2010-12-31")))
+            (loop (cdr l))))))
+
+ (define (to-csv stdata)
+   (let loop ([l stdata])
+    (if (null? l)
+      ""
+      (string-append (format "~A,~A,~A~%" (caar l) (cadar l) (caddar l)) (loop (cdr l))))))
+
+ (load "stability.scm")
+ (display (to-csv STOCK-DATA))
+
+ ;(load "net_worths.scm")
+ ;(associate-net-worths-with-prices NET_WORTHS);)
